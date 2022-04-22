0c24b90bbbb1ac05bff1e168efd985115071de1f
Khailas R <khailas303@gmail.com>
2022-04-22 15:58:33 +0530

    react initial

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <p>Frontend</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HomePage = () => {
+  return (
+    <div>
+        <p>Logged into the HomePage</p>
+    </div>
+  )
+}
+
+export default HomePage;
