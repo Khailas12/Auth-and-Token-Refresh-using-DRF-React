085828479a9e02f3ef611e5c1b30e08cdb9a1f08
Khailas R <khailas303@gmail.com>
2022-04-22 15:03:05 +0530

    authentication initial update

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
-from django.http import JsonResponse
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/token',       # to submit username and pswd
         '/api/token/refresh'       # new token based on the refresh token sent to the backend
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
