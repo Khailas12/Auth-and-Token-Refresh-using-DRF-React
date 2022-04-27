ff22ae35f28e9e0f8e62f9dadbcb80a084d86d8d
Khailas R <khailas303@gmail.com>
2022-04-27 18:23:32 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 
 const App = () => {
     return (
-        <div className="App">
-            <p>Frontend</p>
+        <div className='App'>
+            <Router>
+                <Route component={HomePage} path='/' exact />
+                <Route component={LoginPage} path='login/' />
+            </Router>
         </div>
     );
 }
